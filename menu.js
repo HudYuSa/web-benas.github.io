f0567f5e3f3a5710d3d27d7289ade6a3677203a0
Hud Yusuf Atsigah <hudyusufatsigah@gmail.com>
2021-12-24 15:12:18 +0800

    nambahin file baru dari web benas

---
+++ menu.js
@@ -0,0 +1,10 @@
+var x = document.getElementsByClassName("list1");
+
+const menu = [
+	'Americano',
+	'Espresso'
+]
+
+menu.forEach((i) {
+	var li = '<li class="kotak1"><a href=""></a>${i} <span>:15.000</span></li>' 
+})
